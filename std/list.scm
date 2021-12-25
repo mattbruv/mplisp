6052792fe7db5a99c862cbf70c1c7689e794da5c
Matt P <5638426+mattbruv@users.noreply.github.com>
2021-12-25 00:28:27 -0500

    add basic filter function

--- std/list.scm
+++ std/list.scm
@@ -1,3 +1,8 @@
 
+(define filter-proxy (lambda (fn xs acc)
+    (if (empty? xs) acc
+        (if (fn (car xs))
+            (filter-proxy fn (cdr xs) (cons (car xs) acc))
+            (filter-proxy fn (cdr xs) acc)))))
 
-; (define filter (lambda ()))
+(define filter (lambda (fn xs) (filter-proxy fn xs '())))
--- test/test.scm
+++ test/test.scm
@@ -1,6 +1,8 @@
 
 ;(car '(1 2 3 4))
 
-(empty? (quote ()))
+;(empty? (quote ()))
+(define gt5 (lambda (x) (> x 5)))
+(filter gt5 '(1 2 3 4 5 6 7 8 9 10))
 
 ;(quote (1 2 3 4))
