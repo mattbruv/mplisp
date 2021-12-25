a9a2c159d6f3bb9e9dbd38f556a15004c469a368
Matt P <5638426+mattbruv@users.noreply.github.com>
2021-12-25 00:09:07 -0500

    day 17 progress

--- test/day17.scm
+++ test/day17.scm
@@ -16,9 +16,9 @@
 (define pastY (lambda (y) (< y yMin)))
 (define pastBounds (lambda (x y) (or (pastX x) (pastY y))))
 
-(define step (lambda (x y xVel yVel peak)
+(define step (lambda (x y xVel yVel peak steps)
     ; if we have hit our magic area, return highest peak
-    (if (inBounds x y) peak
+    (if (inBounds x y) (cons peak (cons x (cons y (cons steps '()))))
         ; if we are past the bounds, return FAIL
         (if (pastBounds x y) false
             ; otherwise, do the next step
@@ -28,11 +28,23 @@
                 (if (> xVel 0) (- xVel 1)
                     (if (< xVel 0) (+ xVel 1) 0))
                 (- yVel 1)
-                (if (> y peak) y peak))))))
+                (if (> y peak) y peak)
+                (+ 1 steps))))))
 
-(define shoot (lambda (xVel yVel) (step 0 0 xVel yVel 0)))
+(define shoot (lambda (xVel yVel) (step 0 0 xVel yVel 0 0)))
 
+(define xVelDist (lambda (x) (if (= x 1) 1
+    (+ x (xVelDist (- x 1))))))
+
+(xVelDist 6)
 (shoot 6 9)
+; loop through all possible ranges of X values
+; loop up from zero to all hits of y
+
+; get the max of these lists
+
+; calculate the minimum needed velocity to even reach the target
+; (shoot 3 2000)
 
 
 ;(define range (lambda (from to acc) (if (= from (+ 1 to)) acc
